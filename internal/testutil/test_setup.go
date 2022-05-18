3fc41c0e05761888d615790ab440a17578ecc141
Bastian Doetsch <bastian.doetsch@snyk.io>
2022-05-18 14:25:16 +0200

    refactor: deactivate pact on windows

--- internal/testutil/test_setup.go
+++ internal/testutil/test_setup.go
@@ -2,7 +2,10 @@ package testutil
 
 import (
 	"os"
+	"runtime"
 	"testing"
+
+	"github.com/pact-foundation/pact-go/dsl"
 
 	"github.com/snyk/snyk-ls/config"
 	"github.com/snyk/snyk-ls/internal/progress"
@@ -22,6 +25,22 @@ func IntegTest(t *testing.T) {
 func UnitTest(t *testing.T) {
 	t.Helper()
 	config.CurrentConfig = config.New()
+}
+
+func Pact(t *testing.T, pactDir string, provider string) *dsl.Pact {
+	t.Helper()
+	if runtime.GOOS == "windows" {
+		t.Skipf("We don't have pact on Windows in CI/CD")
+	}
+	pact := &dsl.Pact{
+		Consumer: "SnykLS",
+		Provider: provider,
+		PactDir:  pactDir,
+	}
+	t.Cleanup(func() {
+		pact.Teardown()
+	})
+	return pact
 }
 
 func CreateDummyProgressListener(t *testing.T) {
