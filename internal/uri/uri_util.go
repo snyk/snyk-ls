3286dffbc8b0be979029fb6dfc9c803911074fc8
Bastian Doetsch <bastian.doetsch@snyk.io>
2022-05-18 14:12:00 +0200

    refactor: enable windows runner in integration tests, add URI conversion tests

--- internal/uri/uri_util.go
+++ internal/uri/uri_util.go
@@ -4,35 +4,68 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
+	"unicode"
 
 	sglsp "github.com/sourcegraph/go-lsp"
 )
 
-const uncPrefix = "//"
-const uriPrefix = "file://"
-const eclipseWorkspaceFolderPrefix = "file:"
-
-// PathFromUri converts the given uri to a file path
-// `file:///C:/a/a.txt` is converted to `C:\a\a.txt` on Windows
-// `file:/root/a` is converted to `/root/a` on Linux and macOS. This is necessary due to Eclipse sending wrong URI
-// `file:///root is converted to `/root` on Linux and macOS
-func PathFromUri(uri sglsp.DocumentURI) string {
-	var path = strings.TrimPrefix(string(uri), uriPrefix)
-	if runtime.GOOS == "windows" &&
-		!strings.HasPrefix(path, uncPrefix) { // UNC path
-		path = strings.TrimPrefix(path, "/") // /C:/... --> C:/...
-	}
-	return filepath.Clean(strings.TrimPrefix(path, eclipseWorkspaceFolderPrefix))
-}
-
-func PathToUri(path string) sglsp.DocumentURI {
-	if runtime.GOOS == "windows" &&
-		!strings.HasPrefix(path, "//") {
-		path = "/" + path // this is highly dubious - maybe only works for Java on Windows
-	}
-	return sglsp.DocumentURI("file://" + filepath.ToSlash(path))
-}
+const fileScheme = "file://"
+const eclipseWorkspaceFolderScheme = "file:"
 
 func FolderContains(folderPath string, path string) bool {
 	return strings.HasPrefix(path, folderPath)
+}
+
+// PathFromUri converts the given uri to a file path
+// `file:///C:/a/a.txt` is converted to `C:\a\a.txt` on Windows
+// `file://a/a.txt` is converted to `\\a\a.txt` on Windows
+// `file:/root/a` is converted to `/root/a` on Linux and macOS. This is necessary due to Eclipse sending wrong URI
+// `file:///root is converted to `/root` on Linux and macOS
+func PathFromUri(uri sglsp.DocumentURI) string {
+	var path = removeScheme(uri)
+	if isWindows() && isDriveURI(uri) { // UNC path
+		path = strings.TrimPrefix(path, "/") // /C:/... --> C:/...
+	}
+	return filepath.Clean(path)
+}
+
+// PathToUri converts a path to a DocumentURI
+// "/var/log.txt" -> "file:///var/log.txt"
+// "//var/log.txt -> "file:///var/log.txt
+// Windows:"C:/var/log.txt" -> "file:///C:/var/log.txt"
+// Windows:"C:\var\log.txt" -> "file:///C:/var/log.txt"
+// Windows:"\\var\log.txt" -> "file://var/log.txt"
+// Windows:"//var\log.txt" -> "file://var/log.txt"
+func PathToUri(path string) sglsp.DocumentURI {
+	if isWindows() {
+		if isDrivePath(path) {
+			path = "/" + filepath.Clean(path)
+		} else {
+			// "//share/d$/folder/file" -> "share/d$/folder/file
+			path = strings.TrimPrefix(path, "//")
+			// "\\share\d$\folder\file" -> "share/d$/folder/file
+			path = strings.TrimPrefix(path, "\\\\")
+		}
+	}
+	return sglsp.DocumentURI("file://" + filepath.Clean(filepath.ToSlash(path)))
+}
+
+func isWindows() bool {
+	return runtime.GOOS == "windows"
+}
+
+func isDrivePath(path string) bool {
+	sep := path[2]
+	return len(path) > 2 && unicode.IsLetter(rune(path[0])) && path[1] == ':' && (sep == '/' || sep == '\\')
+}
+
+func isDriveURI(uri sglsp.DocumentURI) bool {
+	prefixLength := len(fileScheme) + 1 // file:// + / [C:/...]
+	return len(uri) > prefixLength && isDrivePath(string(uri[prefixLength:]))
+}
+
+func removeScheme(uri sglsp.DocumentURI) string {
+	var path = strings.TrimPrefix(string(uri), fileScheme)
+	path = strings.TrimPrefix(path, eclipseWorkspaceFolderScheme)
+	return path
 }
--- internal/uri/uri_util_test.go
+++ internal/uri/uri_util_test.go
@@ -1,6 +1,7 @@
 package uri
 
 import (
+	"runtime"
 	"testing"
 
 	"github.com/sourcegraph/go-lsp"
@@ -14,4 +15,38 @@ func TestPathFromUri(t *testing.T) {
 
 func TestPathToUri(t *testing.T) {
 	assert.Equal(t, lsp.DocumentURI("file://asdf"), PathToUri("asdf"))
+	assert.Equal(t, lsp.DocumentURI("file:///asdf"), PathToUri("//asdf"))
+	if runtime.GOOS == "windows" {
+		assert.Equal(t, lsp.DocumentURI("file:///C:/folder/file"), PathToUri("C:\\folder\\file"))
+		assert.Equal(t, lsp.DocumentURI("file:///C:/folder/file"), PathToUri("C:/folder/file"))
+		assert.Equal(t, lsp.DocumentURI("file://share/c$/folder/file"), PathToUri("//shares/c$/folder/file"))
+	}
+}
+
+func TestFolderContains(t *testing.T) {
+	assert.True(t, FolderContains("C:/folder/", "C:/folder/file"))
+	assert.True(t, FolderContains("C:/folder/", "C:/folder/subfolder/file"))
+	assert.False(t, FolderContains("C:/folder/", "C:/otherFolder/file"))
+	assert.False(t, FolderContains("C:/folder/", "D:/folder/file"))
+}
+
+func TestIsDrivePath(t *testing.T) {
+	assert.True(t, isDrivePath("C:/"))
+	assert.True(t, isDrivePath("C:/folder/file"))
+	assert.True(t, isDrivePath("D:/folder/file"))
+	assert.True(t, isDrivePath("D:\\folder\\file"))
+	assert.False(t, isDrivePath("//folder/file"))
+	assert.False(t, isDrivePath("\\\\UNC\\folder\\file"))
+	assert.False(t, isDrivePath("/C:/folder/file"))
+}
+
+func TestIsDriveURI(t *testing.T) {
+	assert.False(t, isDriveURI("/C:/folder/file"))
+	assert.False(t, isDriveURI("C:/folder/file"))
+	assert.False(t, isDriveURI("C:\\folder\\file"))
+	assert.False(t, isDriveURI("\\\\folder\\file"))
+	assert.False(t, isDriveURI("/folder/file"))
+	assert.False(t, isDriveURI("file:///folder/file"))
+	assert.True(t, isDriveURI("file:///C:/folder/file"))
+	assert.True(t, isDriveURI("file:///D:/folder/file"))
 }
