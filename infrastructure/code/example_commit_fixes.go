dd18a059ce8a47a77bf4cbae21896b56ee883f09
Bastian Doetsch <bastian.doetsch@snyk.io>
2022-07-28 16:59:09 -0500

    chore: 💄 remove superfluous new lines between example commit fixes

--- infrastructure/code/example_commit_fixes.go
+++ infrastructure/code/example_commit_fixes.go
@@ -18,12 +18,12 @@ func (c *exampleCommit) toMarkdown() (msg string) {
 	if fixDescription != "" {
 		builder.WriteString(fmt.Sprintf("### [%s](%s)", fixDescription, c.fix.CommitURL))
 	}
-	builder.WriteString("\n\n```\n\n")
+	builder.WriteString("\n```\n")
 	for _, line := range c.fix.Lines {
 		lineChangeChar := c.lineChangeChar(line.LineChange)
 		builder.WriteString(fmt.Sprintf("%s %04d : %s\n", lineChangeChar, line.LineNumber, line.Line))
 	}
-	builder.WriteString("\n\n```\n\n")
+	builder.WriteString("\n```\n")
 	return builder.String()
 }
 
