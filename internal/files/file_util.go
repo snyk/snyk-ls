d87ef131261028a42c549ca38662f448641af1f9
Bastian Doetsch <bastian.doetsch@snyk.io>
2022-07-28 16:46:42 -0500

    chore: add sanity checks to FileUtil

--- internal/files/file_util.go
+++ internal/files/file_util.go
@@ -3,6 +3,8 @@ package files
 import (
 	"os"
 	"strings"
+
+	"github.com/pkg/errors"
 )
 
 type FileUtil struct{}
@@ -13,11 +15,17 @@ func New() *FileUtil {
 
 // GetLineOfCode returns the line of code from file (1-based)
 func (f *FileUtil) GetLineOfCode(filePath string, line int) (string, error) {
+	if line <= 0 {
+		return "", errors.Errorf("invalid line number %d", line)
+	}
 	lines, err := f.readFile(filePath)
+	if err != nil {
+		return "", err
+	}
 	if len(lines) >= line {
 		return lines[line-1], nil
 	}
-	return "", err
+	return "", errors.Errorf("line number above number of lines")
 }
 
 func (f *FileUtil) readFile(filePath string) (lines []string, err error) {
