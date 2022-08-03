00fe6a7bb7c5d2853f65c731ed69b02fdb67bb2d
Bastian Doetsch <bastian.doetsch@snyk.io>
2022-08-03 06:20:42 -0500

    docs: added comment about usage of script

--- getLanguageServer.sh
+++ getLanguageServer.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# This file allows to download the latest language server, which is helpful for integration into non-managed Editors and IDEs.
+# Currently, these might be NeoVIM, Sublime Text or Atom, but any editor that hasn't got a downloader built by us needs to download
+# and update the language server regularly, and this script allows this for system administrators and users.
+
 set -ex
 ARCH=darwin_arm64
 PROTOCOL_VERSION=$(grep "LS_PROTOCOL_VERSION" .goreleaser.yaml | tail -1 | cut -f2 -d "=" |xargs)
