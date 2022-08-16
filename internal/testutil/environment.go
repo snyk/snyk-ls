5257592e0b18fe2122ca332489a0ae4106badc1d
Asaf Agami <asaf.agami@snyk.io>
2022-08-16 11:26:14 +0300

    test: added SetEnvOrFail

--- internal/testutil/environment.go
+++ internal/testutil/environment.go
@@ -1,7 +1,17 @@
 package testutil
 
-import "os"
+import (
+	"os"
+	"testing"
+)
 
 func GetEnvironmentToken() string {
 	return os.Getenv("SNYK_TOKEN")
+}
+
+func SetEnvOrFail(t *testing.T, key string, value string) {
+	err := os.Setenv(key, value)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
