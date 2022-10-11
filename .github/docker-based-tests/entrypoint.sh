8df1088d3061ded9cccc45c8c9e10001da0bd39f
Jason Luong <jason.luong@snyk.io>
2022-10-11 17:36:20 +0100

    Revert "chore: cleanup workflows"
    This reverts commit e30cd1da9f5acfda5ba2f20b0cf34ed0df3cf87c.

--- .github/docker-based-tests/entrypoint.sh
+++ .github/docker-based-tests/entrypoint.sh
@@ -24,7 +24,9 @@ sudo iptables -A OUTPUT -m owner --uid-owner root -j ACCEPT
 sudo iptables -A OUTPUT -m owner --uid-owner proxy -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --dport 80 -j REJECT
 sudo iptables -A OUTPUT -p tcp --dport 443 -j REJECT
+sudo Xvfb -ac :99 -screen 0 1280x1024x24 > /dev/null 2>&1 &
 sleep 5 # wait for squid to startup
 make tools
 export PATH=$PATH:$PWD/.bin/pact/bin:$PWD/.bin/
+export DISPLAY=:99
 exec "$@"
