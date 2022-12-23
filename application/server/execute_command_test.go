443df7ea19c18a756b5aa28bce2f11099fa11cb8
Bastian Doetsch <bastian.doetsch@snyk.io>
2022-12-23 07:42:03 +0100

    feat: add workspaceFolderScan command

--- application/server/execute_command_test.go
+++ application/server/execute_command_test.go
@@ -48,6 +48,22 @@ func Test_executeWorkspaceScanCommand_shouldStartWorkspaceScanOnCommandReceipt(t
 	}, 2*time.Second, time.Millisecond)
 }
 
+func Test_executeWorkspaceFolderScanCommand_shouldStartFolderScanOnCommandReceipt(t *testing.T) {
+	loc := setupServer(t)
+
+	scanner := &snyk.TestScanner{}
+	workspace.Get().AddFolder(workspace.NewFolder("dummy", "dummy", scanner, di.HoverService()))
+
+	params := lsp.ExecuteCommandParams{Command: snyk.WorkspaceFolderScanCommand, Arguments: []any{"dummy"}}
+	_, err := loc.Client.Call(ctx, "workspace/executeCommand", params)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Eventually(t, func() bool {
+		return scanner.Calls() > 0
+	}, 2*time.Second, time.Millisecond)
+}
+
 func Test_executeWorkspaceScanCommand_shouldAskForTrust(t *testing.T) {
 	loc := setupServer(t)
 
