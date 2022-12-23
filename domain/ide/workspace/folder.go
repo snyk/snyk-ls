137300a5754844f970fb6e0618a73ac45c110e61
Bastian Doetsch <bastian.doetsch@snyk.io>
2022-12-23 08:23:08 +0100

    chore: add test to ensure we don't accidentally clear other folders diagnostics

--- domain/ide/workspace/folder.go
+++ domain/ide/workspace/folder.go
@@ -194,7 +194,7 @@ func (f *Folder) filterCachedDiagnostics() (fileIssues map[string][]snyk.Issue) 
 
 func filterIssues(issues []snyk.Issue, supportedIssueTypes map[product.FilterableIssueType]bool) []snyk.Issue {
 	logger := log.With().Str("method", "filterIssues").Logger()
-	filteredIssues := []snyk.Issue{}
+	filteredIssues := make([]snyk.Issue, len(issues))
 
 	for _, issue := range issues {
 		// Logging here might hurt performance, should benchmark if filtering is slow
