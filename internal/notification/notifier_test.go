151d7fd60bc88f36f7ab27ac67dfe45801e70863
Asaf Agami <asaf.agami@snyk.io>
2023-01-08 16:08:32 +0200

    test: tracking messages on MockNotifier

--- internal/notification/notifier_test.go
+++ internal/notification/notifier_test.go
@@ -1,9 +1,13 @@
 package notification_test
 
 import (
+	"fmt"
+
 	sglsp "github.com/sourcegraph/go-lsp"
 
+	"github.com/snyk/snyk-ls/application/server/lsp"
 	"github.com/snyk/snyk-ls/internal/notification"
+	"github.com/snyk/snyk-ls/internal/uri"
 )
 
 var _ notification.Notifier = &MockNotifier{}
@@ -16,17 +20,46 @@ type MockNotifier struct {
 	receiveCounter             int
 	createListenerCounter      int
 	disposeListenerCounter     int
+	sentMessages               []any
 }
+
+func NewMockNotifier() *MockNotifier { return &MockNotifier{} }
 
 func (m *MockNotifier) SendShowMessage(messageType sglsp.MessageType, message string) {
 	m.sendShowMessageCounter++
+	m.sentMessages = append(m.sentMessages, sglsp.ShowMessageParams{
+		Type:    messageType,
+		Message: message,
+	})
 }
 
-func (m *MockNotifier) Send(msg any) { m.sendCounter++ }
+func (m *MockNotifier) Send(msg any) {
+	m.sendCounter++
+	m.sentMessages = append(m.sentMessages, msg)
+}
 
-func (m *MockNotifier) SendError(err error) { m.sendErrorCounter++ }
+func (m *MockNotifier) SendError(err error) {
+	m.sendErrorCounter++
+	m.sentMessages = append(m.sentMessages, sglsp.ShowMessageParams{
+		Type:    sglsp.MTError,
+		Message: fmt.Sprintf("Snyk encountered an error: %v", err),
+	})
+}
 
-func (m *MockNotifier) SendErrorDiagnostic(path string, err error) { m.sendErrorDiagnosticCounter++ }
+func (m *MockNotifier) SendErrorDiagnostic(path string, err error) {
+	m.sendErrorDiagnosticCounter++
+	msg := lsp.PublishDiagnosticsParams{
+		URI: uri.PathToUri(path),
+		Diagnostics: []lsp.Diagnostic{{
+			Range:           sglsp.Range{},
+			Severity:        lsp.DiagnosticsSeverityWarning,
+			Code:            "Snyk Error",
+			CodeDescription: lsp.CodeDescription{Href: "https://snyk.io/user-hub"},
+			Message:         err.Error(),
+		}},
+	}
+	m.sentMessages = append(m.sentMessages, msg)
+}
 
 func (m *MockNotifier) Receive() (payload any, stop bool) {
 	m.receiveCounter++
