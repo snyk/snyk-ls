b668fd13d65d44c595361a78eb6f06389ee5b8a8
michelkaporin <michel@snyk.io>
2023-01-10 14:05:39 +0100

    test: use matrix tests in scan_notifier

--- application/server/notification/scan_notifier_test.go
+++ application/server/notification/scan_notifier_test.go
@@ -7,71 +7,61 @@ import (
 
 	lsp2 "github.com/snyk/snyk-ls/application/server/lsp"
 	notification2 "github.com/snyk/snyk-ls/application/server/notification"
+	"github.com/snyk/snyk-ls/domain/snyk"
 	"github.com/snyk/snyk-ls/internal/notification"
 	"github.com/snyk/snyk-ls/internal/testutil"
 )
 
-const productName = "foo"
-const folderPath = "/test/folderPath"
-
-func Test_SendInProgressMessage_InProgressMessageSent(t *testing.T) {
-	// Arrange
+func Test_SendMessage(t *testing.T) {
 	testutil.UnitTest(t)
-	expectedProductName := productName
-	mockNotifier := notification.NewMockNotifier()
-	scanNotifier, _ := notification2.NewScanNotifier(mockNotifier, expectedProductName)
 
-	// Act
-	scanNotifier.SendInProgress(folderPath)
+	folderPath := "/test/folderPath"
 
-	// Assert
-	for _, msg := range mockNotifier.SentMessages() {
-		scanMessage, ok := msg.(lsp2.SnykScanParams)
-		if ok && scanMessage.Status == lsp2.InProgress && scanMessage.Product == expectedProductName {
-			return
-		}
+	tests := []struct {
+		name           string
+		act            func(scanNotifier snyk.ScanNotifier)
+		expectedStatus lsp2.ScanStatus
+	}{
+		{
+			name: "SendInProgressMessage",
+			act: func(scanNotifier snyk.ScanNotifier) {
+				scanNotifier.SendInProgress(folderPath)
+			},
+			expectedStatus: lsp2.InProgress,
+		},
+		{
+			name: "SendSuccessMessage",
+			act: func(scanNotifier snyk.ScanNotifier) {
+				scanNotifier.SendSuccess(folderPath)
+			},
+			expectedStatus: lsp2.Success,
+		},
+		{
+			name: "SendErrorMessage",
+			act: func(scanNotifier snyk.ScanNotifier) {
+				scanNotifier.SendError(folderPath)
+			},
+			expectedStatus: lsp2.ErrorStatus,
+		},
 	}
-	assert.Fail(t, "Scan message was not sent")
-}
 
-func Test_SendSuccessMessage_SuccessMessageSent(t *testing.T) {
-	// Arrange
-	testutil.UnitTest(t)
-	expectedProductName := productName
-	mockNotifier := notification.NewMockNotifier()
-	scanNotifier, _ := notification2.NewScanNotifier(mockNotifier, expectedProductName)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			expectedProduct := "foo"
+			mockNotifier := notification.NewMockNotifier()
+			scanNotifier, _ := notification2.NewScanNotifier(mockNotifier, expectedProduct)
 
-	// Act
-	scanNotifier.SendSuccess(folderPath)
+			test.act(scanNotifier)
 
-	// Assert
-	for _, msg := range mockNotifier.SentMessages() {
-		scanMessage, ok := msg.(lsp2.SnykScanParams)
-		if ok && scanMessage.Status == lsp2.Success && scanMessage.Product == expectedProductName {
-			return
-		}
+			for _, msg := range mockNotifier.SentMessages() {
+				scanMessage, ok := msg.(lsp2.SnykScanParams)
+				if ok && scanMessage.Status == test.expectedStatus && scanMessage.Product == expectedProduct {
+					return
+				}
+			}
+			assert.Fail(t, "Scan message was not sent")
+		})
 	}
-	assert.Fail(t, "Scan message was not sent")
-}
-
-func Test_SendErrorMessage_ErrorMessageReceived(t *testing.T) {
-	// Arrange
-	testutil.UnitTest(t)
-	expectedProductName := productName
-	mockNotifier := notification.NewMockNotifier()
-	scanNotifier, _ := notification2.NewScanNotifier(mockNotifier, expectedProductName)
-
-	// Act
-	scanNotifier.SendError(folderPath)
-
-	// Assert
-	for _, msg := range mockNotifier.SentMessages() {
-		scanMessage, ok := msg.(lsp2.SnykScanParams)
-		if ok && scanMessage.Status == lsp2.ErrorStatus && scanMessage.Product == expectedProductName {
-			return
-		}
-	}
-	assert.Fail(t, "Scan message was not sent")
 }
 
 func Test_NewScanNotifier_EmptyProductName_Errors(t *testing.T) {
