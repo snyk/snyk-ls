4a87b218a59ab8837e27e0454dd9a4f7fc48244a
Bastian Doetsch <bastian.doetsch@snyk.io>
2023-04-27 09:04:58 +0530

    chore: add mutex to configureLogging to avoid race conditions in tests

--- application/config/config.go
+++ application/config/config.go
@@ -438,6 +438,8 @@ func (c *Config) SetLogPath(logPath string) {
 }
 
 func (c *Config) ConfigureLogging(server lsp.Server) {
+	c.m.Lock()
+	defer c.m.Unlock()
 	var logLevel zerolog.Level
 	var err error
 
