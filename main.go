fc922733d7c89b1c469845bf42633fa4ff11912d
Asaf Agami <asaf.agami@snyk.io>
2023-06-01 17:07:09 +0300

    fix: setting log level via env-var freezes the server [HEAD-317] (#309)

--- main.go
+++ main.go
@@ -43,6 +43,7 @@ func main() {
 			er.FlushErrorReporting()
 		}
 	}()
+
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	c := config.New()
 	config.SetCurrentConfig(c)
@@ -52,15 +53,13 @@ func main() {
 		os.Exit(1)
 	}
 	if output != "" {
-		lines := strings.Split(output, " ")
-		fmt.Println("License information")
-		fmt.Println("Snyk Language Server is licensed under the Apache 2.0 license")
-		fmt.Println("The following dependencies and licenses are used in this project:")
-		for _, line := range lines {
-			fmt.Println(line)
-		}
-		fmt.Println("You can access the detailed license information under https://github.com/snyk/snyk-ls/tree/main/licenses")
+		fmt.Fprintln(os.Stderr, "License information")
+		fmt.Fprintln(os.Stderr, "Snyk Language Server is licensed under the Apache 2.0 license")
+		fmt.Fprintln(os.Stderr, "The following dependencies and licenses are used in this project:")
+		fmt.Fprintln(os.Stderr, strings.ReplaceAll(output, " ", "\n"))
+		fmt.Fprintln(os.Stderr, "You can access the detailed license information under https://github.com/snyk/snyk-ls/tree/main/licenses")
 	}
+
 	log.Info().Msg(config.Version)
 	log.Trace().Interface("environment", os.Environ()).Msg("start environment")
 	server.Start(c)
