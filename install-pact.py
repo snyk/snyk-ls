3998fed65a67faee3a02c2356efdf39ad34cb0f5
Bastian Doetsch <bastian.doetsch@snyk.io>
2023-09-06 09:08:38 +0200

    fix: install into correct directory

--- install-pact.py
+++ install-pact.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import platform
+import tarfile
 
 import requests
 
@@ -9,48 +10,55 @@ def download_and_extract(url, destination):
 	response = requests.get(url, stream=True)
 	response.raise_for_status()
 
+	os.makedirs(os.path.dirname(destination), exist_ok=True)
+
 	with open(destination, 'wb') as f:
 		for chunk in response.iter_content(chunk_size=8192):
 			if chunk:
 				f.write(chunk)
+
+	# Extract the contents of the downloaded tar.gz archive to the pact_dir
+	with tarfile.open(destination, 'r:gz') as tar:
+		tar.extractall(path=os.path.dirname(destination))
 
 
 def main():
 	script_dir = os.path.dirname(os.path.realpath(__file__))
 	pact_dir = os.path.join(script_dir, ".bin", "pact")
 
-	if os.path.isdir(pact_dir):
-		print("--- 🛠 Pact CLI already installed")
-		return
+	is_installed = os.path.isdir(pact_dir)
+	if not is_installed:
+		print("--- 🛠 Installing Pact CLI dependencies")
+		os_name = platform.system().lower()
+		arch = platform.machine()
 
-	print("--- 🛠 Installing Pact CLI dependencies")
-	os_name = platform.system().lower()
-	arch = platform.machine()
+		if os_name == "linux" and arch == "x86_64":
+			os_name = "linux-x86_64"
+		elif os_name == "darwin" and arch == "x86_64":
+			os_name = "osx-x86_64"
+		elif os_name == "darwin" and arch == "arm64":
+			os_name = "osx-arm64"
+		else:
+			print("Sorry, you'll need to install the pact-ruby-standalone manually.")
+			exit(1)
 
-	if os_name == "linux" and arch == "x86_64":
-		os_name = "linux-x86_64"
-	elif os_name == "darwin" and arch == "x86_64":
-		os_name = "osx-x86_64"
-	elif os_name == "darwin" and arch == "arm64":
-		os_name = "osx-arm64"
+		tag_url = "https://github.com/pact-foundation/pact-ruby-standalone/releases/latest"
+		response = requests.head(tag_url, allow_redirects=True)
+		response.raise_for_status()
+		tag = response.url.split("/")[-1]
+		filename = f"pact-{tag[1:]}-{os_name}.tar.gz"
+		download_url = f"https://github.com/pact-foundation/pact-ruby-standalone/releases/download/{tag}/{filename}"
+		download_and_extract(download_url, os.path.join(script_dir, ".bin", filename))
 	else:
-		print("Sorry, you'll need to install the pact-ruby-standalone manually.")
-		exit(1)
+		print("--- 🛠 Pact CLI already installed; skipping download")
 
-	tag_url = "https://github.com/pact-foundation/pact-ruby-standalone/releases/latest"
-	response = requests.head(tag_url, allow_redirects=True)
-	response.raise_for_status()
-	tag = response.url.split("/")[-1]
-	filename = f"pact-{tag[1:]}-{os_name}.tar.gz"
-	download_url = f"https://github.com/pact-foundation/pact-ruby-standalone/releases/download/{tag}/{filename}"
-
-	download_and_extract(download_url, filename)
+	print("--- 🛠 Creating symlinks for Pact CLI")
 	os.makedirs("/usr/local/bin", exist_ok=True)
 
 	binaries = [
 		"pact", "pact-broker", "pact-message", "pact-mock-service",
 		"pact-plugin-cli", "pact-provider-verifier", "pact-publish",
-		"pact-stub-service", "pact-flow"
+		"pact-stub-service", "pactflow"
 	]
 
 	for binary in binaries:
@@ -58,8 +66,9 @@ def main():
 		dest_path = os.path.join("/usr/local/bin", binary)
 		try:
 			os.symlink(source_path, dest_path)
+			print(f"Created symlink for {binary} at {dest_path}")
 		except OSError:
-			print(f"Error creating symlink for {binary}; skipped.")
+			print(f"Cannot create symlink for {binary}. It most likely already exists. Please check {dest_path}")
 
 	print("--- 🛠 Pact CLI installed")
 
