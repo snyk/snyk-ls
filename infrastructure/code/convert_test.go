9d9fbfed968127727c5804c58c25c65e3572b04c
Catalina Oyaneder <cat2608@gmail.com>
2024-04-03 16:48:06 +0100

    fix: update converter test with Data FLow in a table

--- infrastructure/code/convert_test.go
+++ infrastructure/code/convert_test.go
@@ -645,7 +645,10 @@ func TestSnykCodeBackendService_convert_shouldConvertIssues(t *testing.T) {
 	assert.Equal(t, resp.Sarif.Runs[0].Tool.Driver.Rules[0].Properties.Cwe, issue.CWEs)
 	assert.Nil(t, issues[0].IgnoreDetails)
 	assert.False(t, issues[0].IsIgnored)
-	assert.Contains(t, (issues[0].AdditionalData).(snyk.CodeIssueData).Details, "<h2>Data Flow - 4 steps</h2>")
+
+	codeIssueData, _ := issues[0].AdditionalData.(snyk.CodeIssueData)
+	dataFlowSteps := fmt.Sprintf(`<h2 class="data-flow-header">Data Flow - %d steps</h2>`, len(codeIssueData.DataFlow))
+	assert.Contains(t, codeIssueData.Details, dataFlowSteps)
 }
 
 func referencesForSampleSarifResponse() []snyk.Reference {
