5616255d94976e40435b82a40d8b4e8d678aafa9
Bastian Doetsch <bastian.doetsch@snyk.io>
2024-05-21 13:25:10 +0200

    fix: merge errors

--- infrastructure/oss/issue_html_test.go
+++ infrastructure/oss/issue_html_test.go
@@ -25,7 +25,6 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/snyk/snyk-ls/application/config"
 	"github.com/snyk/snyk-ls/domain/snyk"
 	"github.com/snyk/snyk-ls/internal/testutil"
 )
