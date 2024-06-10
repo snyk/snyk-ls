aec12f511bd9d66ebee0dd6aac2a26917b9ff400
Teodora Sandu <teodora.sandu@snyk.io>
2024-06-10 10:53:14 +0100

    refactor: modularise svg icon

--- infrastructure/code/code_html.go
+++ infrastructure/code/code_html.go
@@ -105,6 +105,7 @@ func getCodeDetailsHtml(issue snyk.Issue) string {
 		"LessonIcon":         getLessonIconSvg(),
 		"IgnoreLineAction":   getLineToIgnoreAction(issue),
 		"HasAIFix":           additionalData.HasAIFix,
+		"ExternalIcon":       getExternalIconSvg(),
 	}
 
 	if issue.IsIgnored {
@@ -227,6 +228,14 @@ func getRepoName(commitURL string) string {
 func formatDate(date time.Time) string {
 	month := date.Format("January")
 	return fmt.Sprintf("%s %02d, %d", month, date.Day(), date.Year())
+}
+
+func getExternalIconSvg() template.HTML {
+	return template.HTML(` <svg class="is-external-icon" width="9" height="9" viewBox="0 0 9 9" xmlns="http://www.w3.
+org/2000/svg" fill="none">
+          <path d="M4.99998 0L6.64648 1.6465L3.14648 5.1465L3.85348 5.8535L7.35348 2.3535L8.99998 4V0H4.99998Z" fill="#888"/>
+          <path d="M8 8H1V1H4.5L3.5 0H1C0.4485 0 0 0.4485 0 1V8C0 8.5515 0.4485 9 1 9H8C8.5515 9 9 8.5515 9 8V5.5L8 4.5V8Z" fill="#888"/>
+        </svg>`)
 }
 
 func getSeverityIconSvg(issue snyk.Issue) template.HTML {
