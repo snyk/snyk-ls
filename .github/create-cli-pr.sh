9287ef57483f6e76285049476942e7dbadd3d2e4
Bastian Doetsch <bastian.doetsch@snyk.io>
2024-06-20 08:23:55 +0200

    feat: add commits in PR body

--- .github/create-cli-pr.sh
+++ .github/create-cli-pr.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #
 # © 2024 Snyk Limited
 #
@@ -14,30 +15,37 @@
 # limitations under the License.
 #
 
-#!/bin/bash
 set -ex
 
 CLI_DIR=$(mktemp -d)
-gh repo clone git@github.com:snyk/cli.git $CLI_DIR -- --depth=1
-pushd $CLI_DIR
+
+
+gh repo clone git@github.com:snyk/cli.git "$CLI_DIR" -- --depth=1
+pushd "$CLI_DIR/cliv2"
+  LS_COMMIT_HASH=$(grep snyk-ls go.mod| cut -d "-" -f 4)
+popd
+
+WHAT_CHANGED=$(git whatchanged "$LS_COMMIT_HASH"...HEAD)
+BODY=$(printf "## Changes since last integration of Language Server\n\n\`\`\`\n%s\n\`\`\`" "$WHAT_CHANGED")
+
+pushd "$CLI_DIR"
   UPGRADE=$(go run scripts/upgrade-snyk-go-dependencies.go --name=snyk-ls)
-  LS_VERSION=$(echo $UPGRADE | sed 's/.*Sha: \(.*\) URL.*/\1/')
-  BRANCH=feat/automatic-upgrade-of-ls-to-$LS_VERSION
-  BASE=$(git log --pretty=tformat:"%h" -n1 .)
-  git checkout -b $BRANCH
+  NEW_LS_VERSION=$(echo "$UPGRADE" | sed 's/.*Sha: \(.*\) URL.*/\1/')
+  BRANCH=feat/automatic-upgrade-of-ls-to-$NEW_LS_VERSION
+  git checkout -b "$BRANCH"
 
   git config --global user.email "team-ide@snyk.io"
   git config --global user.name "Snyk Team IDE"
   git config --global gpg.format ssh
   git config --global commit.gpgsign true
 
-  echo $PUB_SIGNING_KEY > signingkey.pub
+  echo "$PUB_SIGNING_KEY" > signingkey.pub
   git config --global user.signingkey ./signingkey.pub
 
-  git commit -am "feat: automatic integration of language server $LS_VERSION"
-  git push --set-upstream origin $BRANCH
+  git commit -am "feat: automatic integration of language server $NEW_LS_VERSION"
+  git push --set-upstream origin "$BRANCH"
 
-  COMMIT_HASH=$(git log --pretty=tformat:"%h" -n1 .)
-  gh pr create --repo github.com/snyk/cli --base main --head $BRANCH --title "feat(language-server): integrate LS ($LS_VERSION)" --body "$(echo $UPGRADE | sed 's/.*Message: \(.*\) URL.*$/\1/')"
+
+  gh pr create --repo github.com/snyk/cli --base main --head "$BRANCH" --title "feat(language-server): integrate LS ($NEW_LS_VERSION)" --body "$BODY"
   gh pr merge -m --auto
 popd
