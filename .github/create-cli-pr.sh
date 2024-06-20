e5886002c90d110901d320db6610ee6b97213d09
Bastian Doetsch <bastian.doetsch@snyk.io>
2024-06-20 11:41:48 +0200

    fix: revert breaking linter changes (#555)

--- .github/create-cli-pr.sh
+++ .github/create-cli-pr.sh
@@ -18,9 +18,7 @@
 set -ex
 
 CLI_DIR=$(mktemp -d)
-
-
-gh repo clone git@github.com:snyk/cli.git "$CLI_DIR" -- --depth=1
+gh repo clone git@github.com:snyk/cli.git $CLI_DIR -- --depth=1
 pushd "$CLI_DIR/cliv2"
   LS_COMMIT_HASH=$(grep snyk-ls go.mod| cut -d "-" -f 4)
 popd
@@ -28,24 +26,23 @@ popd
 WHAT_CHANGED=$(git whatchanged "$LS_COMMIT_HASH"...HEAD)
 BODY=$(printf "## Changes since last integration of Language Server\n\n\`\`\`\n%s\n\`\`\`" "$WHAT_CHANGED")
 
-pushd "$CLI_DIR"
+pushd $CLI_DIR
   UPGRADE=$(go run scripts/upgrade-snyk-go-dependencies.go --name=snyk-ls)
-  NEW_LS_VERSION=$(echo "$UPGRADE" | sed 's/.*Sha: \(.*\) URL.*/\1/')
-  BRANCH=feat/automatic-upgrade-of-ls-to-$NEW_LS_VERSION
-  git checkout -b "$BRANCH"
+  LS_VERSION=$(echo $UPGRADE | sed 's/.*Sha: \(.*\) URL.*/\1/')
+  BRANCH=feat/automatic-upgrade-of-ls-to-$LS_VERSION
+  git checkout -b $BRANCH
 
   git config --global user.email "team-ide@snyk.io"
   git config --global user.name "Snyk Team IDE"
   git config --global gpg.format ssh
   git config --global commit.gpgsign true
 
-  echo "$PUB_SIGNING_KEY" > signingkey.pub
+  echo $PUB_SIGNING_KEY > signingkey.pub
   git config --global user.signingkey ./signingkey.pub
 
-  git commit -am "feat: automatic integration of language server $NEW_LS_VERSION"
-  git push --set-upstream origin "$BRANCH"
+  git commit -am "feat: automatic integration of language server $LS_VERSION"
+  git push --set-upstream origin $BRANCH
 
-
-  gh pr create --repo github.com/snyk/cli --base main --head "$BRANCH" --title "feat(language-server): integrate LS ($NEW_LS_VERSION)" --body "$BODY"
+  gh pr create --repo github.com/snyk/cli --base main --head $BRANCH --title "feat(language-server): integrate LS ($LS_VERSION)" --body "$BODY"
   gh pr merge -m --auto
 popd
