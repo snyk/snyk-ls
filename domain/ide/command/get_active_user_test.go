29e42c3f6e40e3a5a391bec48c0686fd146afc9c
Bastian Doetsch <bastian.doetsch@snyk.io>
2024-07-10 08:52:05 +0200

    fix: tests

--- domain/ide/command/get_active_user_test.go
+++ domain/ide/command/get_active_user_test.go
@@ -39,6 +39,21 @@ import (
 
 func Test_getActiveUser_Execute_User_found(t *testing.T) {
 	c := testutil.UnitTest(t)
+	cmd := setupCommandWithAuthService(c)
+
+	expectedUser, expectedUserData := whoamiWorkflowResponse(t)
+
+	mockEngine, engineConfig := setUpEngineMock(t, c)
+	mockEngine.EXPECT().GetConfiguration().Return(engineConfig).AnyTimes()
+	mockEngine.EXPECT().InvokeWithConfig(localworkflows.WORKFLOWID_WHOAMI, gomock.Any()).Return(expectedUserData, nil)
+
+	actualUser, err := cmd.Execute(context.Background())
+
+	assert.NoErrorf(t, err, "cmd.Execute() error = %v", err)
+	assert.Equal(t, expectedUser, actualUser)
+}
+
+func setupCommandWithAuthService(c *config.Config) *getActiveUser {
 	provider := authentication.NewFakeCliAuthenticationProvider(c)
 	provider.IsAuthenticated = true
 
@@ -54,28 +69,13 @@ func Test_getActiveUser_Execute_User_found(t *testing.T) {
 			notification.NewNotifier(),
 		),
 	}
-
-	expectedUser, expectedUserData := whoamiWorkflowResponse(t)
-
-	mockEngine, engineConfig := setUpEngineMock(t, c)
-	mockEngine.EXPECT().GetConfiguration().Return(engineConfig).AnyTimes()
-	mockEngine.EXPECT().InvokeWithConfig(localworkflows.WORKFLOWID_WHOAMI, gomock.Any()).Return(expectedUserData, nil)
-
-	actualUser, err := cmd.Execute(context.Background())
-
-	assert.NoErrorf(t, err, "cmd.Execute() error = %v", err)
-	assert.Equal(t, expectedUser, actualUser)
+	return cmd
 }
 
 func Test_getActiveUser_Execute_Result_Empty(t *testing.T) {
-	testutil.UnitTest(t)
-	cmd := &getActiveUser{
-		command: types.CommandData{
-			CommandId: types.GetActiveUserCommand,
-		},
-	}
+	c := testutil.UnitTest(t)
+	cmd := setupCommandWithAuthService(c)
 
-	c := config.CurrentConfig()
 	mockEngine, engineConfig := setUpEngineMock(t, c)
 	mockEngine.EXPECT().GetConfiguration().Return(engineConfig).AnyTimes()
 	mockEngine.EXPECT().InvokeWithConfig(localworkflows.WORKFLOWID_WHOAMI, gomock.Any()).Return([]workflow.Data{}, nil)
@@ -87,14 +87,9 @@ func Test_getActiveUser_Execute_Result_Empty(t *testing.T) {
 }
 
 func Test_getActiveUser_Execute_Error_Result(t *testing.T) {
-	testutil.UnitTest(t)
-	cmd := &getActiveUser{
-		command: types.CommandData{
-			CommandId: types.GetActiveUserCommand,
-		},
-	}
+	c := testutil.UnitTest(t)
+	cmd := setupCommandWithAuthService(c)
 
-	c := config.CurrentConfig()
 	mockEngine, engineConfig := setUpEngineMock(t, c)
 	mockEngine.EXPECT().GetConfiguration().Return(engineConfig).AnyTimes()
 	testError := errors.New("test error")
