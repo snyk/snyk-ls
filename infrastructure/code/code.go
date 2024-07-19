b819ebc2dfe881862c17b87934ef1b8a729f3eaa
Abdelrahman Shawki Hassan <shawki.hassan@snyk.io>
2024-07-19 20:33:29 +0200

    fix: send notification in a go routine

--- infrastructure/code/code.go
+++ infrastructure/code/code.go
@@ -193,7 +193,7 @@ func (sc *Scanner) Scan(ctx context.Context, path string, folderPath string) (is
 	if err == nil && c.IsDeltaFindingsEnabled() {
 		baseScanErr := scanAndPersistBaseBranch(ctx, sc, folderPath)
 		if baseScanErr != nil {
-			sc.notifier.SendShowMessage(sglsp.MTError, "Couldn't determine the diff between current and base branch. Falling back to showing full scan results.")
+			go sc.notifier.SendShowMessage(sglsp.MTError, "Couldn't determine the diff between current and base branch. Falling back to showing full scan results.")
 			logger.Error().Err(baseScanErr).Msg("couldn't scan base branch for folder " + folderPath)
 		}
 	}
