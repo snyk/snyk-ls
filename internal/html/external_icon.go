ca51b7bcdba97f3612b934754f0145edb5546610
Cata <catalina.oyaneder@snyk.io>
2024-08-09 12:52:12 +0100

    refactor: move SVGs to files [IDE-289] (#620)

--- internal/html/external_icon.go
+++ internal/html/external_icon.go
@@ -16,22 +16,11 @@
 
 package html
 
-import (
-	"html/template"
-	"strings"
+import "html/template"
 
-	"github.com/gomarkdown/markdown"
-)
-
-func MarkdownToHTML(md string) template.HTML {
-	html := markdown.ToHTML([]byte(md), nil, nil)
-	return template.HTML(html)
-}
-
-func IdxMinusOne(n int) int {
-	return n - 1
-}
-
-func TrimCWEPrefix(cwe string) string {
-	return strings.TrimPrefix(cwe, "CWE-")
+func ExternalIcon() template.HTML {
+	return template.HTML(`<svg class="is-external-icon" width="9" height="9" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg" fill="none">
+		<path d="M4.99998 0L6.64648 1.6465L3.14648 5.1465L3.85348 5.8535L7.35348 2.3535L8.99998 4V0H4.99998Z" fill="#888"/>
+		<path d="M8 8H1V1H4.5L3.5 0H1C0.4485 0 0 0.4485 0 1V8C0 8.5515 0.4485 9 1 9H8C8.5515 9 9 8.5515 9 8V5.5L8 4.5V8Z" fill="#888"/>
+	</svg>`)
 }
