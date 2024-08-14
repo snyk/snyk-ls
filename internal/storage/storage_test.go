ed7908759bc1305dcecb8aca6f86a406b5f35591
Bastian Doetsch <bastian.doetsch@snyk.io>
2024-08-14 08:42:01 +0200

    fix: re-add defer to go func

--- internal/storage/storage_test.go
+++ internal/storage/storage_test.go
@@ -62,14 +62,16 @@ func Test_ParallelFileLocking(t *testing.T) {
 		for i := 0; i < parallelism; i++ {
 			go func(count int) {
 				cut, _ := NewStorageWithCallbacks(WithStorageFile(file))
+
 				lockErr := cut.Lock(context.Background(), time.Millisecond)
 				require.NoError(t, lockErr)
+				defer func() {
+					unlockErr := cut.Unlock()
+					require.NoError(t, unlockErr)
+				}()
 
 				err = cut.Set(fmt.Sprintf("test-%d", count), count)
 				require.NoError(t, err)
-
-				unlockErr := cut.Unlock()
-				require.NoError(t, unlockErr)
 			}(i)
 		}
 
