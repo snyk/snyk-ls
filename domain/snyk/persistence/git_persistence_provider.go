1c76bbb72ce08ac395d15ed4537b065d6e75c955
Abdelrahman Shawki Hassan <shawki.hassan@snyk.io>
2024-08-27 10:58:00 +0200

    fix: dont load expired files on init

--- domain/snyk/persistence/git_persistence_provider.go
+++ domain/snyk/persistence/git_persistence_provider.go
@@ -24,14 +24,11 @@ import (
 	"path/filepath"
 	"strings"
 	"sync"
-	"time"
 
 	"github.com/rs/zerolog"
-
 	"github.com/snyk/snyk-ls/domain/snyk"
 	"github.com/snyk/snyk-ls/internal/product"
 	"github.com/snyk/snyk-ls/internal/util"
-	"github.com/snyk/snyk-ls/internal/vcs"
 )
 
 const (
@@ -104,9 +101,13 @@ func (g *GitPersistenceProvider) Init(folderPaths []string) error {
 		}
 
 		for _, filePath := range filePaths {
-			_, hash, commitHash, p, fileParseErr := g.fileSchema(filePath)
-			if fileParseErr != nil {
-				g.logger.Error().Err(fileParseErr).Send()
+			schemaVersion, hash, commitHash, p, fileParseErr := g.fileSchema(filePath)
+			if fileParseErr != nil || g.isExpired(schemaVersion, filePath) {
+				g.logger.Info().Msgf("file %s is expired. attempting to delete", filePath)
+				err = g.deleteFile(filePath)
+				if err != nil {
+					g.logger.Error().Err(err).Msgf("failed to delete file %s", filePath)
+				}
 				continue
 			}
 			g.createOrAppendToCache(hash, commitHash, p)
@@ -126,7 +127,7 @@ func (g *GitPersistenceProvider) Clear(folders []string, deleteOnlyExpired bool)
 	}
 	for _, folderPath := range folders {
 		g.logger.Info().Msgf("checking for expired cache for folder %s", folderPath)
-		cacheDir, err := g.snykCacheFolder(folderPath)
+		cacheDir, err := g.snykCacheDir(folderPath)
 		if err != nil {
 			continue
 		}
@@ -161,7 +162,7 @@ func (g *GitPersistenceProvider) GetPersistedIssueList(folderPath string, p prod
 	g.mutex.Lock()
 	defer g.mutex.Unlock()
 
-	cacheDir, err := g.snykCacheFolder(folderPath)
+	cacheDir, err := g.snykCacheDir(folderPath)
 	if err != nil {
 		g.logger.Error().Err(err).Msgf("failed to determine cache dir in path %s", folderPath)
 		return nil, err
@@ -200,7 +201,7 @@ func (g *GitPersistenceProvider) Add(folderPath, commitHash string, issueList []
 	g.mutex.Lock()
 	defer g.mutex.Unlock()
 
-	cacheDir, err := g.snykCacheFolder(folderPath)
+	cacheDir, err := g.snykCacheDir(folderPath)
 	if err != nil {
 		g.logger.Error().Err(err).Msgf("failed to determine cache dir in path %s", folderPath)
 		return err
@@ -234,7 +235,7 @@ func (g *GitPersistenceProvider) Exists(folderPath, commitHash string, p product
 	g.mutex.Lock()
 	defer g.mutex.Unlock()
 
-	cacheDir, err := g.snykCacheFolder(folderPath)
+	cacheDir, err := g.snykCacheDir(folderPath)
 	if err != nil {
 		g.logger.Error().Err(err).Msgf("failed to determine cache dir in path %s", folderPath)
 		return false
@@ -277,17 +278,8 @@ func (g *GitPersistenceProvider) fileSchema(fullPath string) (string, hashedFold
 func (g *GitPersistenceProvider) deleteCacheEntryIfExpired(fullPath string) error {
 	g.logger.Debug().Msgf("deleting cached scan file %s", fullPath)
 	schemaVersion, hash, commitHash, p, _ := g.fileSchema(fullPath)
-	if schemaVersion != SchemaVersion {
-		return g.deleteFromDiskAndCache(fullPath, hash, commitHash, p)
-	}
-
-	// Check last modified date
-	fileInfo, err := os.Stat(fullPath)
-	if err != nil {
-		return err
-	}
-	// If elapsed time is > ExpirationInSeconds, delete the file
-	if time.Since(fileInfo.ModTime()) > time.Duration(ExpirationInSeconds)*time.Second {
+	isExpired := g.isExpired(schemaVersion, fullPath)
+	if isExpired {
 		return g.deleteFromDiskAndCache(fullPath, hash, commitHash, p)
 	}
 
@@ -295,8 +287,7 @@ func (g *GitPersistenceProvider) deleteCacheEntryIfExpired(fullPath string) erro
 }
 
 func (g *GitPersistenceProvider) deleteFromDiskAndCache(fullPath string, hash hashedFolderPath, commitHash string, p product.Product) error {
-	// if file has incorrect schema we just delete it
-	err := os.Remove(fullPath)
+	err := g.deleteFile(fullPath)
 	if err != nil {
 		g.logger.Debug().Err(err).Msg("could not remove cached file from disk: " + fullPath)
 	}
@@ -305,15 +296,6 @@ func (g *GitPersistenceProvider) deleteFromDiskAndCache(fullPath string, hash ha
 		g.logger.Debug().Err(err).Msg("failed to remove file from cache: " + fullPath)
 	}
 	return err
-}
-
-func (g *GitPersistenceProvider) deleteFile(fullPath string) error {
-	g.logger.Debug().Msgf("deleting cached scan file %s", fullPath)
-	err := os.Remove(fullPath)
-	if err != nil {
-		return err
-	}
-	return nil
 }
 
 func (g *GitPersistenceProvider) deleteFromCache(hash hashedFolderPath, commitHash string, p product.Product) error {
@@ -334,14 +316,6 @@ func (g *GitPersistenceProvider) deleteFromCache(hash hashedFolderPath, commitHa
 	delete(g.cache[hash], p)
 
 	return nil
-}
-
-func (g *GitPersistenceProvider) snapshotExistsOnDisk(cacheDir string, hash hashedFolderPath, commitHash string, p product.Product) bool {
-	filePath := getLocalFilePath(cacheDir, hash, commitHash, p)
-	if _, err := os.Stat(filePath); os.IsNotExist(err) {
-		return false
-	}
-	return true
 }
 
 func (g *GitPersistenceProvider) getCommitHashForProduct(folderPath string, p product.Product) (commitHash string, err error) {
@@ -410,65 +384,4 @@ func (g *GitPersistenceProvider) createOrAppendToCache(pathHash hashedFolderPath
 	}
 	pchm[product] = commitHash
 	g.cache[pathHash] = pchm
-}
-
-func (g *GitPersistenceProvider) getPersistedFiles(cacheDir string) (persistedFiles []string, err error) {
-	entries, err := os.ReadDir(cacheDir)
-	if err != nil {
-		return persistedFiles, err
-	}
-
-	for _, entry := range entries {
-		fileName := entry.Name()
-		if !strings.HasSuffix(fileName, ".json") {
-			continue
-		}
-		s := strings.Split(fileName, ".")
-		if len(s) == 5 {
-			persistedFiles = append(persistedFiles, fileName)
-		}
-	}
-	return persistedFiles, nil
-}
-
-func (g *GitPersistenceProvider) persistToDisk(cacheDir string, folderHashedPath hashedFolderPath, commitHash string, p product.Product, inputToCache []snyk.Issue) error {
-	filePath := getLocalFilePath(cacheDir, folderHashedPath, commitHash, p)
-	data, err := json.Marshal(inputToCache)
-	if err != nil {
-		return err
-	}
-	g.logger.Debug().Msg("persisting scan results in file " + filePath)
-	return os.WriteFile(filePath, data, 0644)
-}
-
-func (g *GitPersistenceProvider) ensureCacheDirExists(folderPath string) (string, error) {
-	g.logger.Debug().Msg("attempting to determine .git folder path")
-	cacheDir, err := g.snykCacheFolder(folderPath)
-	if err != nil {
-		return "", err
-	}
-
-	if _, err = os.Stat(cacheDir); os.IsNotExist(err) {
-		err = os.Mkdir(cacheDir, 0700)
-		if err != nil {
-			return "", err
-		}
-	}
-	return cacheDir, nil
-}
-
-func (g *GitPersistenceProvider) snykCacheFolder(folderPath string) (string, error) {
-	gitFolder, err := vcs.GitRepoFolderPath(g.logger, folderPath)
-	if err != nil {
-		return "", err
-	}
-
-	cacheDir := filepath.Join(gitFolder, CacheFolder)
-
-	return cacheDir, nil
-}
-
-func getLocalFilePath(cacheDir string, folderPathHash hashedFolderPath, commitHash string, p product.Product) string {
-	productName := p.ToProductCodename()
-	return filepath.Join(cacheDir, fmt.Sprintf("%s.%s.%s.%s.json", SchemaVersion, folderPathHash, commitHash, productName))
 }
