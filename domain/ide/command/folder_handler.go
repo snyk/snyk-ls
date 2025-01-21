7062d2fc8fcf64de3d2faa207a10dbea2084e17a
DariusZdroba <darius-beniamin.zdroba@snyk.io>
2025-01-21 10:14:29 +0200

    feat: send initial summary panel notification

--- domain/ide/command/folder_handler.go
+++ domain/ide/command/folder_handler.go
@@ -19,6 +19,7 @@ package command
 import (
 	"context"
 	"fmt"
+	"github.com/snyk/snyk-ls/internal/summary"
 
 	"github.com/snyk/snyk-ls/domain/snyk/persistence"
 	noti "github.com/snyk/snyk-ls/internal/notification"
@@ -34,10 +35,21 @@ const DontTrust = "Don't trust folders"
 
 func HandleFolders(ctx context.Context, srv types.Server, notifier noti.Notifier, persister persistence.ScanSnapshotPersister) {
 	go sendFolderConfigsNotification(notifier)
+	go sendInitialSummaryPanelNotification(notifier)
 	initScanPersister(persister)
 	HandleUntrustedFolders(ctx, srv)
 }
-
+func sendInitialSummaryPanelNotification(notifier noti.Notifier) {
+	c := config.CurrentConfig()
+	logger := c.Logger().With().Str("method", "sendInitialSummaryPanelNotification").Logger()
+	renderer, err := summary.NewHtmlRenderer(c)
+	if err != nil {
+		logger.Error().Err(err).Msg("could not create html renderer")
+		return
+	}
+	scanSummaryParams := types.ScanSummary{ScanSummary: renderer.GetDetailsHtml()}
+	notifier.Send(scanSummaryParams)
+}
 func sendFolderConfigsNotification(notifier noti.Notifier) {
 	c := config.CurrentConfig()
 	ws := c.Workspace()
