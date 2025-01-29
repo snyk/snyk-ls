e30c238c50471d6fd06e0c99d83a56f033110454
Abdelrahman Shawki Hassan <shawki.hassan@snyk.io>
2025-01-29 14:22:16 +0100

    fix: smoke test delta wait for results

--- application/server/server_smoke_test.go
+++ application/server/server_smoke_test.go
@@ -866,7 +866,11 @@ func Test_SmokeSnykCodeDelta_NewVulns(t *testing.T) {
 
 	waitForDeltaScan(t, scanAggregator)
 	checkForScanParams(t, jsonRPCRecorder, cloneTargetDir, product.ProductCode)
-	issueList := getIssueListFromPublishDiagnosticsNotification(t, jsonRPCRecorder, product.ProductCode, cloneTargetDir)
+	var issueList []types.ScanIssue
+	assert.Eventually(t, func() bool {
+		issueList = getIssueListFromPublishDiagnosticsNotification(t, jsonRPCRecorder, product.ProductCode, cloneTargetDir)
+		return len(issueList) > 0
+	}, maxIntegTestDuration, 2*time.Millisecond)
 
 	assert.True(t, len(issueList) > 0)
 	assert.Contains(t, issueList[0].FilePath, fileWithNewVulns)
