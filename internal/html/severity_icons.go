715a39c268ee50faf550b6b26fa3411b3ffe1c87
Abdelrahman Shawki Hassan <shawki.hassan@snyk.io>
2025-03-07 17:29:01 +0100

    chore: lint

--- internal/html/severity_icons.go
+++ internal/html/severity_icons.go
@@ -25,7 +25,7 @@ import (
 func SeverityIcon(issue types.Issue) template.HTML {
 	switch issue.GetSeverity() {
 	case types.Critical:
-		 return template.HTML(`<svg id="severity-icon" class="severity-icon critical" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="16" cy="16" r="16" fill="#AB1A1A"/><path d="M16.1752 11C17.98 11 19.5173 12.0972 20.1001 13.6333L21.9989 13.0004C21.1514 10.6698 18.8639 9 16.1752 9C12.7648 9 10.0001 11.6863 10.0001 15V17C10.0001 20.3137 12.7648 23 16.175 23C18.8639 23 21.1514 21.3302 21.9989 18.9996L20.1001 18.3667C19.5173 19.9028 17.98 21 16.175 21C16.172 21 16.1689 21 16.1659 21C13.8641 20.9956 12.0001 19.2064 12.0001 17V15C12.0001 12.7909 13.8694 11 16.1752 11Z" fill="white"/></svg>`)
+		return template.HTML(`<svg id="severity-icon" class="severity-icon critical" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="16" cy="16" r="16" fill="#AB1A1A"/><path d="M16.1752 11C17.98 11 19.5173 12.0972 20.1001 13.6333L21.9989 13.0004C21.1514 10.6698 18.8639 9 16.1752 9C12.7648 9 10.0001 11.6863 10.0001 15V17C10.0001 20.3137 12.7648 23 16.175 23C18.8639 23 21.1514 21.3302 21.9989 18.9996L20.1001 18.3667C19.5173 19.9028 17.98 21 16.175 21C16.172 21 16.1689 21 16.1659 21C13.8641 20.9956 12.0001 19.2064 12.0001 17V15C12.0001 12.7909 13.8694 11 16.1752 11Z" fill="white"/></svg>`)
 	case types.High:
 		return template.HTML(`<svg id="severity-icon" class="severity-icon high" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="16" cy="16" r="16" fill="#D93600"/><path d="M11 10H13V15H19V10H21V22H19V17H13V22H11V10Z" fill="white"/></svg>`)
 	case types.Medium:
