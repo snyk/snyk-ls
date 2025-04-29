68c23b2c8d3d3983f5a6b3a995ae321702b9cf8a
Ben Durrans <Benjamin.Durrans@snyk.io>
2025-04-29 09:39:55 +0100

    fix: add datepicker polyfill
    Stopped sending IAW form if pending
    Reverted `toggleElement` changes from previous commit.
    Manually added the polyfill license and prevent automatic removal of it. Previously license files that were added manually would be deleted by the GitHub action.

--- infrastructure/code/template/scripts.js
+++ infrastructure/code/template/scripts.js
@@ -1,6 +1,20 @@
 function _createForOfIteratorHelper(r, e) { var t = "undefined" != typeof Symbol && r[Symbol.iterator] || r["@@iterator"]; if (!t) { if (Array.isArray(r) || (t = _unsupportedIterableToArray(r)) || e && r && "number" == typeof r.length) { t && (r = t); var _n = 0, F = function F() {}; return { s: F, n: function n() { return _n >= r.length ? { done: !0 } : { done: !1, value: r[_n++] }; }, e: function e(r) { throw r; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var o, a = !0, u = !1; return { s: function s() { t = t.call(r); }, n: function n() { var r = t.next(); return a = r.done, r; }, e: function e(r) { u = !0, o = r; }, f: function f() { try { a || null == t.return || t.return(); } finally { if (u) throw o; } } }; }
 function _unsupportedIterableToArray(r, a) { if (r) { if ("string" == typeof r) return _arrayLikeToArray(r, a); var t = {}.toString.call(r).slice(8, -1); return "Object" === t && r.constructor && (t = r.constructor.name), "Map" === t || "Set" === t ? Array.from(r) : "Arguments" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? _arrayLikeToArray(r, a) : void 0; } }
 function _arrayLikeToArray(r, a) { (null == a || a > r.length) && (a = r.length); for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e]; return n; }
+function toggleElement(element, toggle) {
+  if (!element) {
+    return;
+  }
+  if (toggle === 'show') {
+    element.className = element.className.replace(/(?:^|\s)hidden(?!\S)/g, '');
+  } else if (toggle === 'hide') {
+    if (element.className.indexOf('hidden') === -1) {
+      element.className += ' hidden';
+    }
+  } else {
+    console.error('Unexpected toggle value', toggle);
+  }
+}
 function dispatchEvent(element, dispatchEventName) {
   if (!element) {
     return;
@@ -27,8 +41,8 @@ retryGenerateFixButton === null || retryGenerateFixButton === void 0 || retryGen
 applyFixButton === null || applyFixButton === void 0 || applyFixButton.addEventListener('click', applyFix);
 function generateAIFix() {
   if (!suggestion) return;
-  generateAIFixButton.classList.add('hidden');
-  fixLoadingIndicatorElem.classList.remove('hidden');
+  toggleElement(generateAIFixButton, 'hide');
+  toggleElement(fixLoadingIndicatorElem, 'show');
   var folderPath = generateAIFixButton.getAttribute('folder-path');
   var filePath = generateAIFixButton.getAttribute('file-path');
   var generateFixQueryString = folderPath + '@|@' + filePath + '@|@' + issueId;
@@ -73,8 +87,8 @@ function nextDiff() {
 }
 function retryGenerateAIFix() {
   console.log('retrying generate AI Fix');
-  fixWrapperElem.classList.remove('hidden');
-  fixErrorSectionElem.classList.add('hidden');
+  toggleElement(fixWrapperElem, 'show');
+  toggleElement(fixErrorSectionElem, 'hide');
   generateAIFix();
 }
 function previousDiff() {
@@ -133,17 +147,17 @@ function showCurrentDiff() {
   // Some IDEs send back the suggestion, others send the suggestion.diffs directly.
   var showSuggestion = getSuggestion();
   if (!showSuggestion.length) {
-    noDiffsElem.classList.remove('hidden');
-    diffTopElem.classList.add('hidden');
-    diffElem.classList.add('hidden');
-    applyFixButton.classList.add('hidden');
+    toggleElement(noDiffsElem, 'show');
+    toggleElement(diffTopElem, 'hide');
+    toggleElement(diffElem, 'hide');
+    toggleElement(applyFixButton, 'hide');
     return;
   }
   if (!showSuggestion.length || diffSelectedIndex < 0 || diffSelectedIndex >= showSuggestion.length) return;
-  noDiffsElem.classList.add('hidden');
-  diffTopElem.classList.remove('hidden');
-  diffElem.classList.remove('hidden');
-  applyFixButton.classList.remove('hidden');
+  toggleElement(noDiffsElem, 'hide');
+  toggleElement(diffTopElem, 'show');
+  toggleElement(diffElem, 'show');
+  toggleElement(applyFixButton, 'show');
   diffNumElem.innerText = showSuggestion.length.toString();
   diffNum2Elem.innerText = showSuggestion.length.toString();
   diffSelectedIndexElem.innerText = (diffSelectedIndex + 1).toString();
@@ -154,7 +168,7 @@ function showCurrentDiff() {
   if(diffSuggestion.explanation.length){
     var explainFixHeader = document.getElementById("ai-explain-header")
     if(explainFixHeader){
-      explainFixHeader.classList.remove('hidden');
+      toggleElement(explainFixHeader, 'show')
     }
   }
   fixExplainText.innerText = diffSuggestion.explanation;
@@ -179,15 +193,15 @@ var ignoreCreateButton = document.getElementById('ignore-create');
 if (ignoreFormContainer !== null && ignoreFormContainer !== void 0 && ignoreCreateButton !== null && ignoreCreateButton !== void 0) {
   // Open form button
   ignoreCreateButton.addEventListener('click', function() {
-    ignoreCreateButton.classList.add('hidden');
-    ignoreFormContainer.classList.remove('hidden');
+    toggleElement(ignoreCreateButton, 'hide');
+    toggleElement(ignoreFormContainer, 'show');
   });
 
   // Cancel button
   var ignoreFormCancelButton = document.getElementById('ignore-form-cancel');
   ignoreFormCancelButton.addEventListener('click', function() {
-    ignoreFormContainer.classList.add('hidden');
-    ignoreCreateButton.classList.remove('hidden');
+    toggleElement(ignoreFormContainer, 'hide');
+    toggleElement(ignoreCreateButton, 'show');
   });
 
   // Submit button
@@ -197,7 +211,7 @@ if (ignoreFormContainer !== null && ignoreFormContainer !== void 0 && ignoreCrea
     var ignoreExpirationType = document.getElementById('ignore-form-expiration-type').value;
     var ignoreExpirationDate = '';
     if (ignoreExpirationType === 'custom-expiration-date') {
-      ignoreExpirationDate = document.getElementById('ignore-form-expiration-date').value;
+      ignoreExpirationDate = new Date(document.getElementById('ignore-form-expiration-date').value).toISOString().split('T')[0];
     }
     var ignoreReason = document.getElementById('ignore-form-ignore-reason').value;
     ${ideSubmitIgnoreRequest}
@@ -208,9 +222,9 @@ if (ignoreFormContainer !== null && ignoreFormContainer !== void 0 && ignoreCrea
   var ignoreFormExpirationDate = document.getElementById('ignore-form-expiration-date');
   ignoreFormExpirationType.addEventListener('change', function(event) {
     if (event.target.value === 'do-not-expire') {
-      ignoreFormExpirationDate.classList.add('hidden');
+      toggleElement(ignoreFormExpirationDate, 'hide');
     } else {
-      ignoreFormExpirationDate.classList.remove('hidden');
+      toggleElement(ignoreFormExpirationDate, 'show');
     }
   });
 
@@ -220,9 +234,9 @@ if (ignoreFormContainer !== null && ignoreFormContainer !== void 0 && ignoreCrea
     if (event.target.value === 'temporary-ignore') {
       ignoreFormExpirationType.value = 'custom-expiration-date';
       dispatchEvent(ignoreFormExpirationType, 'change');
-      ignoreFormExpirationType.classList.add('hidden');
+      toggleElement(ignoreFormExpirationType, 'hide');
     } else {
-      ignoreFormExpirationType.classList.remove('hidden');
+      toggleElement(ignoreFormExpirationType, 'show');
     }
   });
 }
