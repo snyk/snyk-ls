846fe14a436de98a1e2872f4613244f6e20b82b3
Abdelrahman Shawki Hassan <shawki.hassan@snyk.io>
2025-04-29 17:08:31 +0200

    fix: disable explain

--- infrastructure/code/ai_fix_handler.go
+++ infrastructure/code/ai_fix_handler.go
@@ -39,10 +39,11 @@ type AiFixHandler struct {
 type AiStatus string
 
 const (
-	AiFixNotStarted AiStatus = "NOT_STARTED"
-	AiFixInProgress AiStatus = "IN_PROGRESS"
-	AiFixSuccess    AiStatus = "SUCCESS"
-	AiFixError      AiStatus = "ERROR"
+	AiFixNotStarted  AiStatus = "NOT_STARTED"
+	AiFixInProgress  AiStatus = "IN_PROGRESS"
+	AiFixSuccess     AiStatus = "SUCCESS"
+	AiFixError       AiStatus = "ERROR"
+	shouldRunExplain          = false
 )
 const (
 	ExplainApiVersion string = "2024-10-15"
@@ -74,6 +75,9 @@ func (fixHandler *AiFixHandler) GetResults(fixId string) (filePath string, diff 
 }
 
 func (fixHandler *AiFixHandler) EnrichWithExplain(ctx context.Context, c *config.Config, issue types.Issue, suggestions []AutofixUnifiedDiffSuggestion) {
+	if !shouldRunExplain {
+		return
+	}
 	logger := c.Logger().With().Str("method", "EnrichWithExplain").Logger()
 	if ctx.Err() != nil {
 		logger.Debug().Msgf("EnrichWithExplain context canceled")
