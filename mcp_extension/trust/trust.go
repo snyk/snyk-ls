7c1b53e3d24c9d2622ca0d8ca08b1958e7f54ec3
Abdelrahman Shawki Hassan <shawki.hassan@snyk.io>
2025-06-11 17:01:20 +0200

    chore: lint

--- mcp_extension/trust/trust.go
+++ mcp_extension/trust/trust.go
@@ -229,7 +229,7 @@ func (t *FolderTrust) addHttpHandlers(logger zerolog.Logger, mux *http.ServeMux,
 			return
 		}
 		logger.Info().Str("path", folderPath).Msg("User chose not to trust folder")
-		logger.Info().Msg("Operation cancelled by user.")
-		errorChan <- fmt.Errorf("user cancelled trust operation for path: %s", folderPath)
+		logger.Info().Msg("Operation canceled by user.")
+		errorChan <- fmt.Errorf("user canceled trust operation for path: %s", folderPath)
 	})
 }
