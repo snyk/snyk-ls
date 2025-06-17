3d68a24e5928a5afb1c030c78888d109ff899315
Abdelrahman Shawki Hassan <shawki.hassan@snyk.io>
2025-06-17 12:17:11 +0200

    fix: mcp command assignment

--- mcp_extension/trust/trust.go
+++ mcp_extension/trust/trust.go
@@ -105,13 +105,20 @@ func (t *FolderTrust) TrustedFolders() []string {
 }
 
 func (t *FolderTrust) trustedFolders() []string {
-	rawRes := t.config.Get(TrustedFoldersConfigKey)
-	trustedFolders, ok := rawRes.([]string)
-	if !ok {
-		t.logger.Error().Msg("incorrect type stored for trusted folders")
-		return []string{}
+	result := t.config.Get(TrustedFoldersConfigKey)
+	switch v := result.(type) {
+	case []string:
+		return v
+	case []interface{}:
+		var trustedFolders []string
+		for _, item := range v {
+			if folderPath, ok := item.(string); ok {
+				trustedFolders = append(trustedFolders, folderPath)
+			}
+		}
+		return trustedFolders
 	}
-	return trustedFolders
+	return []string{}
 }
 
 func (t *FolderTrust) AddTrustedFolder(folder string) {
