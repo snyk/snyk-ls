3a5a8b8eb77205e60d7f5d17b2e3c8481342098c
Andrew Robinson Hodges <andrew.robinsonhodges@snyk.io>
2025-06-23 14:44:25 +0100

    feat: add pat support (#886)
    * feat: add pat support
    
    * fix: pat url handling for connect button
    
    * fix: rename oauth to oauth2 in lsp types to align with ide changes
    
    * fix: incorrect token sometimes used when auth method changed
    
    * test: add tests for pat authenticator
    
    * chore: update gaf to version with pat compatibility
    
    * fix: lint
    
    * fix: false positive api key detection in test code
    
    * fix: typo inverted logic in setting oauth token
    
    * fix: set authentication method on unit test for auth configuration
    
    * test: fix up tests to handle different authentication methods
    
    * lint: fix up variable assignment in test code
    
    * test: make pat_provider_test more robust
    
    * fix: fix issue where we could get into a loop if integration tests were initialised with no token
    
    * fix: use configured endpoint for PAT auth; ensure token updates are processed first
    
    * fix: update api endpoint before token, in case a logout is triggered
    
    * fix: code review markups
    
    * fix: revert renaming oauth authentication type from oauth2
    
    * fix: typo in oauth check
    
    * fix: need to set oauth mehtod in tests as they default to fake auth
    
    * chore: lint - reorder imports
    
    * fix: incorrect mutex locking in authenticator
    
    * chore: code review markups
    
    * chore: temporarily rmeove commit hashes from gitleaksignore to allow for easier squashing

--- infrastructure/authentication/auth_configuration_test.go
+++ infrastructure/authentication/auth_configuration_test.go
@@ -24,13 +24,13 @@ import (
 	"testing"
 	"time"
 
+	"github.com/snyk/go-application-framework/pkg/auth"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/oauth2"
 
-	"github.com/snyk/go-application-framework/pkg/auth"
-
 	storage2 "github.com/snyk/snyk-ls/internal/storage"
 	"github.com/snyk/snyk-ls/internal/testutil"
+	"github.com/snyk/snyk-ls/internal/types"
 )
 
 func Test_NewOAuthProvider_registersStorageCallback(t *testing.T) {
@@ -67,6 +67,7 @@ func Test_NewOauthProvider_oauthProvider_created_with_injected_refreshMethod(t *
 	storageWithCallbacks, err2 := storage2.NewStorageWithCallbacks(storage2.WithStorageFile(t.TempDir() + "testStorage"))
 	assert.NoError(t, err2)
 	c.SetStorage(storageWithCallbacks)
+	c.SetAuthenticationMethod(types.OAuthAuthentication)
 
 	// an expired token that's set into the configuration
 	token := oauth2.Token{
