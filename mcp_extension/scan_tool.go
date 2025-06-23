72c5d2a0bf6870bd3adb6bfe3ad45aa44edb5e6b
Abdelrahman Shawki Hassan <shawki.hassan@snyk.io>
2025-06-23 14:50:56 +0200

    feat(mcp): enhance logger capabilities (#904)

--- mcp_extension/scan_tool.go
+++ mcp_extension/scan_tool.go
@@ -77,6 +77,7 @@ func loadMcpToolsFromJson() (*SnykMcpTools, error) {
 
 func (m *McpLLMBinding) addSnykTools(invocationCtx workflow.InvocationContext) error {
 	config, err := loadMcpToolsFromJson()
+
 	if err != nil || config == nil {
 		m.logger.Err(err).Msg("Failed to load Snyk tools configuration")
 		return err
@@ -117,14 +118,14 @@ func (m *McpLLMBinding) runSnyk(ctx context.Context, invocationCtx workflow.Invo
 	logger.Debug().Strs("args", command.Args).Str("workingDir", command.Dir).Msg("Running Command with")
 	logger.Trace().Strs("env", command.Env).Msg("Environment")
 
-	command.Stderr = invocationCtx.GetEnhancedLogger()
+	command.Stderr = logger
 	res, err := command.Output()
 	resAsString := string(res)
 
 	logger.Debug().Str("result", resAsString).Msg("Command run result")
 
 	if err != nil {
-		m.logger.Err(err).Msg("Received error running command")
+		logger.Err(err).Msg("Received error running command")
 	}
 	return resAsString, nil
 }
