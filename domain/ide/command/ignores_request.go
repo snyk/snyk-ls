66a007fbd814ab9bc3a405b227b7ad7530f6faec
Ben Durrans <Benjamin.Durrans@snyk.io>
2025-10-23 16:50:05 +0100

    fix: use folder org for IAW edits and deletes [IDE-1501] (#1031)
    Although these code paths are unused, we don't want to forget them, so I am fixing them now.

--- domain/ide/command/ignores_request.go
+++ domain/ide/command/ignores_request.go
@@ -166,6 +166,11 @@ func (cmd *submitIgnoreRequest) initializeEditConfigurations(gafConfig configura
 		return nil, err
 	}
 
+	folderOrg := cmd.c.FolderOrganization(contentRoot)
+	if folderOrg != "" {
+		gafConfig.Set(configuration.ORGANIZATION, folderOrg)
+	}
+
 	gafConfig = initializeBaseConfiguration(gafConfig, contentRoot)
 	gafConfig = addCreateAndUpdateConfiguration(gafConfig, ignoreType, reason, expiration)
 	gafConfig.Set(ignore_workflow.IgnoreIdKey, ignoreId)
@@ -194,6 +199,11 @@ func (cmd *submitIgnoreRequest) initializeDeleteConfiguration(gafConfig configur
 	ignoreId, ok := cmd.command.Arguments[2].(string)
 	if !ok {
 		return nil, fmt.Errorf("ignoreId should be a string")
+	}
+
+	folderOrg := cmd.c.FolderOrganization(contentRoot)
+	if folderOrg != "" {
+		gafConfig.Set(configuration.ORGANIZATION, folderOrg)
 	}
 
 	gafConfig = initializeBaseConfiguration(gafConfig, contentRoot)
