2d451d9a56cc1b82324851e544a6d1bb3b3a8913
Bastian Doetsch <bastian.doetsch@snyk.io>
2025-11-10 12:24:21 +0100

    fix: linter package name issue with os dependent files

--- infrastructure/cli/filename/filename_windows.go
+++ infrastructure/cli/filename/filename_windows.go
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+// Package filename implements os specific filename functionality
 package filename
 
 const ExecutableName = "snyk-win.exe"
