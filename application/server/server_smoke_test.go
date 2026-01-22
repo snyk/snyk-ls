8e7a8185bfac4bb04d67aa3b52be7e7bf13e19c0
Abdelrahman Shawki Hassan <shawki.hassan@snyk.io>
2026-01-22 11:19:54 +0100

    fix: test assertion

--- application/server/server_smoke_test.go
+++ application/server/server_smoke_test.go
@@ -714,7 +714,6 @@ func checkOnlyOneCodeLens(t *testing.T, jsonRPCRecorder *testsupport.JsonRPCReco
 			// "cfenv": "^1.0.4", 1 fixable issue
 			if lens.Range.Start.Line == 19 {
 				assert.Contains(t, lens.Command.Title, "and fix ")
-				assert.NotContains(t, lens.Command.Title, "and fix ")
 			}
 
 			// "tap": "^11.1.3", 12 fixable, 11 unfixable
